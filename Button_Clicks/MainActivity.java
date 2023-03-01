8cbc178eb03f4c2b2435606c11ad83e41d14a84a
Adithi <adithibhatindia@gmail.com>
2023-03-01 05:40:42 +0530

    Create MainActivity.java

---
+++ Button_Clicks/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int clicks  = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view){
+        TextView text = (TextView) findViewById(R.id.text);
+        clicks++;
+        text.setText("Button Clicked"+clicks+" times");
+
+    }
+
+}
